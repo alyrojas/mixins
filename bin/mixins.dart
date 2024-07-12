3fdc6604e3c3acb8ed2378892af4a562ea127e1d
alyrojas <alynerojas@gmail.com>
2024-07-11 22:44:59 -0600

    ejercicios

---
+++ bin/mixins.dart
@@ -0,0 +1,20 @@
+//Crear un mixin
+mixin Volador{
+  void volar(){
+    print("Estoy volando");
+  }
+}
+
+mixin Corredor{
+  void correr(){
+    print("Estoy corriendo");
+  }
+}
+
+class Pajaro with Volador, Corredor{}
+
+void main(){
+  var pajaro = Pajaro();
+  pajaro.volar();
+  pajaro.correr();
+}
